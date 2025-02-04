65133e51b6691e81915666b432405c0dc0536102
Error404Hong <hongjx0321@gmail.com>
2025-02-04 15:04:18 +0800

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,7 +16,6 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  //jingxin
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
